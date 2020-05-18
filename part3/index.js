f147339a0a73405319ae3c70a381165cd02f0844
elljayhaitch <laurajayneharvey@outlook.com>
2020-05-18 22:25:39 +0100

    Reversing person service work - misunderstood exercise

--- part3/index.js
+++ part3/index.js
@@ -1,8 +1,9 @@
-const PersonsService = require('./src/services/personsService');
-
 const express = require('express')
 var morgan = require('morgan')
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
 
 app.use(express.json())
 
@@ -18,13 +19,28 @@ const loggingFormat = (tokens, req, res) => [
 ].join(' ')
 app.use(morgan(loggingFormat))
 
-let persons = [];
-PersonsService
-  .getAll()
-  .then(results => {
-    persons = results
-  })
-  .catch(error => { });
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4
+  }
+]
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
