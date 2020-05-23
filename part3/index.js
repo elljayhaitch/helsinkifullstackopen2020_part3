e9d4a28ebc28c590f58b5bea1513a5219d279ee7
elljayhaitch <laurajayneharvey@outlook.com>
2020-05-23 23:29:06 +0100

    Exercises 3.13 and 3.14

--- part3/index.js
+++ part3/index.js
@@ -1,8 +1,11 @@
-const express = require('express')
-var morgan = require('morgan')
-const app = express()
-const cors = require('cors')
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const Person = require('./models/person');
+var morgan = require('morgan');
 
+// app
+const app = express()
 app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
@@ -19,68 +22,33 @@ const loggingFormat = (tokens, req, res) => [
 ].join(' ')
 app.use(morgan(loggingFormat))
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4
-  }
-]
-
-app.get('/', (req, res) => {
-  res.send(build/index.html)
+app.get('/', (request, response) => {
+  response.send(build/index.html)
 })
 
-app.get('/info', (req, res) => {
-  res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
+app.get('/info', (request, response) => {
+  Person.find({}).then(persons => {
+    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
+  })
 })
 
-app.get('/api/persons', (req, res) => {
-  res.json(persons)
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
+  Person.findById(request.params.id).then(person => {
     response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndRemove(request.params.id).then(person => {
+    response.status(204).end()
+  })
 })
-
-const generateId = () => {
-  let id = 1;
-
-  while (persons.some(person => person.id == id)) {
-    id = Math.floor(Math.random() * 1000) + 1;
-  }
-
-  return id;
-}
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -91,27 +59,11 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  if (!body.number) {
-    return response.status(400).json({
-      error: 'number is missing'
-    })
-  }
+  const person = new Person(body)
 
-  if (persons.some(person => person.name == body.name)) {
-    return response.status(400).json({
-      error: 'name must be unique'
-    })
-  }
-
-  const person = {
-    name: body.name,
-    number: body.number,
-    id: generateId(),
-  }
-
-  persons = persons.concat(person)
-
-  response.json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
 // middleware after routes
@@ -120,7 +72,7 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
